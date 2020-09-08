867bffb5bad9c6e0d312770f1a9f1f8855a3fea6
jooyeal <34053954+jooyeal@users.noreply.github.com>
2020-09-08 22:33:46 +0900

    Connect Database & html -> pug

--- index.js
+++ index.js
@@ -1,14 +1,62 @@
 const express = require('express');
-const path = require('path');
 const app = express();
-app.use(express.static(path.join(__dirname, 'public')));
+const db = require('./lib/db.js');
+const bodyParser = require('body-parser');
+
+db.connect();
+
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+app.set('view engine', 'pug');
+app.set('views', './views');
+
+
+
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public/html', 'index.html'));
+    res.render('index');
 });
 
-app.get('/signup', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public/html', 'sign_up.html'));
+app.get('/sign_up', (req, res) => {
+    res.render('sign-up');
 });
+app.get('/main', (req, res) => {
+    res.render('main');
+});
+
+app.post('/main', (req, res) => {
+    const userId = req.body.userId;
+    const userPw = req.body.userPw;
+    db.query(`select * from member where id = ${userId} and password = ${userPw}`, (err, rows) => {
+        if (err) throw err;
+        if (rows[0] !== undefined) {
+            res.render('main');
+        } else {
+            res.send('no data');
+        }
+    })
+});
+
+
+app.post('/success_sign_up', (req, res) => {
+    const id = req.body.id;
+    const password = req.body.password;
+    const passwordRe = req.body.passwordRe;
+    const name = req.body.name;
+    const email = req.body.email;
+    const date = req.body.date;
+    const phone = req.body.phone;
+    if (password !== passwordRe) {
+        res.send('please check your password ');
+    } else {
+        db.query(`insert into member values(?,?,?,?,?,?)`, [id, password, name, email, date, phone], (err, rows) => {
+            if (err) throw err;
+        })
+        res.render('success-sign-up');
+    }
+});
+
 app.listen(3000, () => {
     console.log('Express App on port 3000!');
 });
